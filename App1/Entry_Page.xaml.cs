777dbdfaf6a37ccd7f4c89d4a71f385a986abef4
opilane <opilane@HP-CZC2349HT9>
2023-02-03 11:44:05 +0200

    Add project files.

---
+++ App1/Entry_Page.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entry_Page : ContentPage
+    {
+        Editor ed;
+        Button btn;
+        public Entry_Page()
+        {
+            //InitializeComponent();
+            ed= new Editor
+            {
+                Placeholder="Sisesta siia tekst",
+                BackgroundColor= Color.White,
+                TextColor= Color.Plum
+            };
+            btn = new Button
+            {
+                Text = "Tagasi"
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = {ed},
+                BackgroundColor = Color.Pink
+            };
+            Content = st;
+            btn.Clicked +=Btn_Clicked;
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+    }
+
+}
