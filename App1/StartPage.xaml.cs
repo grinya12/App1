9ec66802ebd1c17baf72e0cc9dc08efbaed15d74
opilane <opilane@HP-CZC2349HT9>
2023-02-10 11:14:57 +0200

    second lesson_1

--- App1/StartPage.xaml.cs
+++ App1/StartPage.xaml.cs
@@ -27,15 +27,27 @@ namespace App1
                 TextColor = Color.Tomato,
                 BackgroundColor = Color.Violet
             };
+            Button Box_btn = new Button
+            {
+                Text = "Ava BoxView leht",
+                TextColor = Color.Tomato,
+                BackgroundColor = Color.Violet
+            };
             StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { Entry_btn, Timer_btn },
+                Children = { Entry_btn, Timer_btn, Box_btn},
                 BackgroundColor = Color.Yellow
             };
             Content=st;
             Entry_btn.Clicked += Entry_btn_Clicked;
             Timer_btn.Clicked += Timer_btn_Clicked;
+            Box_btn.Clicked += Box_btn_Clicked;
+        }
+
+        private async void Box_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BoxView_Page());
         }
 
         private async void Timer_btn_Clicked(object sender, EventArgs e)
